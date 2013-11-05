442d381304abca4e99412c4c08ddbdd02865a607
JimVanG <jjvg@me.com>
2013-11-05 12:39:12 -0500

    Added blank methods

--- src/Maze.java
+++ src/Maze.java
@@ -1,11 +1,29 @@
-
+/*
+ * James Van Gaasbeck
+ * PID: J2686979
+ * COP 3503 : Computer Science II
+ * Professor Sean Szumlanski
+ * Assignment 6 : Maze Creation
+ * Due: Sunday, November 17, 11:59pm
+ * 
+ */
 public class Maze {
 
-	/**
-	 * @param args
-	 */
+	public static char[][] create(int width, int height) {
+
+		return null;
+	}
+
+	public static double difficultyRating() {
+		return 3;
+
+	}
+
+	public static double hoursSpent() {
+		return 5;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
