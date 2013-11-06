17ce4f1fcc42648dcd1aad6c4488d1c6158da078
JimVanG <jjvg@me.com>
2013-11-06 16:00:20 -0500

    cell & wall class's

--- src/Maze.java
+++ src/Maze.java
@@ -1,3 +1,7 @@
+import java.awt.Point;
+
+import javax.xml.ws.Endpoint;
+
 /*
  * James Van Gaasbeck
  * PID: J2686979
@@ -8,23 +12,69 @@
  * 
  */
 public class Maze {
-
+	public static final Point START_POINT = new Point(0, 0);
+	public static final Point END_POINT = null;
+	
 	public static char[][] create(int width, int height) {
+		
 
 		return null;
 	}
 
 	public static double difficultyRating() {
 		return 3;
-
 	}
 
 	public static double hoursSpent() {
 		return 5;
 	}
+	
+
 
 	public static void main(String[] args) {
 
 	}
 
+	
+	class Wall{
+		
+		char sign;
+		boolean isDestructable;
+		Point position;
+		
+		Wall(boolean isDestructable, Point position){
+			this.sign = '#';
+			this.isDestructable = isDestructable;
+			this.position = position;
+		}
+		
+	}
+
+	class Cell{
+		
+		char sign;
+		Point position;
+		boolean isStart;
+		boolean isEnd;
+		
+		Cell(Point position, boolean isStart, boolean isEnd){
+			this.position = position;
+			if(this.position.equals(START_POINT)){
+				this.isEnd = false;
+				this.isStart = true;
+				this.sign = 's';
+			}else if(this.position.equals(END_POINT)){
+				this.isEnd = true;
+				this.isStart = false;
+				this.sign = 'e';
+			}else{
+				this.isEnd = false;
+				this.isStart = false;
+				this.sign = ' ';
+			}
+		}
+	}
+	
 }
+
+
