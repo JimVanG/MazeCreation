be7c34b68d7f10497979a89fce0f720b92d74746
JimVanG <jjvg@me.com>
2013-11-10 17:57:12 -0500

    Completed Disjoint class

--- src/Maze.java
+++ src/Maze.java
@@ -1,7 +1,4 @@
 import java.awt.Point;
-import java.util.ArrayList;
-
-import javax.xml.ws.Endpoint;
 
 /*
  * James Van Gaasbeck
@@ -17,6 +14,7 @@ public class Maze {
 		NORTH_WEST, WEST, SOUTH_WEST, SOUTH, SOUTH_EAST, EAST, NORTH_EAST, NORTH, CENTER
 	}
 
+	static Cell[] cellList;
 
 	public static char[][] create(int width, int height) {
 
@@ -25,7 +23,7 @@ public class Maze {
 
 		Cell[][] theCells = new Cell[horizontalLength][verticalLength];
 		char[][] theMaze = new char[horizontalLength][verticalLength];
-		Cell[] cellList = new Cell[width * height];
+		cellList = new Cell[width * height];
 
 		int cellX = -1;
 		for (int x = 0; x < horizontalLength; x++) {
@@ -46,25 +44,43 @@ public class Maze {
 				if (x % 2 == 1 && y % 2 == 1) {
 					theCells[x][y] = new Cell(new Point(cellX, cellY++), width,
 							height);
-					
+
 				}
 			}
-			// System.out.println();
 		}
+
+		// print out the walls/cells, and add the cells to an array.
 		int numberOfCells = 0;
 		for (int x = 0; x < verticalLength; x++) {
-			
+
 			for (int y = 0; y < horizontalLength; y++) {
 				System.out.print(theMaze[y][x]);
-				//System.out.print(theCells[x][y].sign);
-				if (x % 2 == 1 && y % 2 == 1){
+				if (x % 2 == 1 && y % 2 == 1) {
+					// print the cell to make the maze complete
 					System.out.print(theCells[y][x].sign);
-					cellList[numberOfCells++] = theCells[y][x];
+					// add the cell to our array of cells (the array will be
+					// used for the DisjointSet)
+					cellList[numberOfCells] = theCells[y][x];
+					// give the cell a number (0 to n-1)
+					cellList[numberOfCells].cellNumber = numberOfCells;
+					// we need to initialize the parent array(cellList), do this
+					// by setting the parent property to the current cellNumber,
+					// this is essentially the "makeSet()" function of a
+					// disjointSet
+					cellList[numberOfCells].parent = numberOfCells;
+					DisjointSet.findSet(numberOfCells++);
 				}
 			}
-
 			System.out.println();
 		}
+
+		DisjointSet.union(0, 1);
+		DisjointSet.union(1, 2);
+		DisjointSet.union(2, 3);
+		DisjointSet.union(3, 4);
+		DisjointSet.union(0, 5);
+		DisjointSet.union(5, 6);
+		DisjointSet.findSet(3);
 
 		return null;
 	}
@@ -103,7 +119,7 @@ public class Maze {
 			case NORTH_WEST:
 				east = new Wall(true);
 				south = new Wall(true);
-				
+
 				north = new Wall(false);
 				west = new Wall(false);
 				break;
@@ -111,27 +127,27 @@ public class Maze {
 				east = new Wall(true);
 				south = new Wall(true);
 				north = new Wall(true);
-				
+
 				west = new Wall(false);
 				break;
 			case SOUTH_WEST:
 				east = new Wall(true);
 				north = new Wall(true);
-				
+
 				south = new Wall(false);
 				west = new Wall(false);
 				break;
 			case SOUTH:
 				west = new Wall(true);
 				east = new Wall(true);
-				south = new Wall(true);
-				
+				north = new Wall(true);
+
 				south = new Wall(false);
 				break;
 			case SOUTH_EAST:
 				north = new Wall(true);
 				west = new Wall(true);
-				
+
 				south = new Wall(false);
 				east = new Wall(false);
 				break;
@@ -145,7 +161,7 @@ public class Maze {
 			case NORTH_EAST:
 				west = new Wall(true);
 				south = new Wall(true);
-				
+
 				north = new Wall(false);
 				east = new Wall(false);
 				break;
@@ -153,7 +169,7 @@ public class Maze {
 				west = new Wall(true);
 				east = new Wall(true);
 				south = new Wall(true);
-				
+
 				north = new Wall(false);
 				break;
 			case CENTER:
@@ -168,15 +184,25 @@ public class Maze {
 			}
 		}
 
+		@Override
+		public String toString() {
+			// show what walls can be destroyed
+			return "North:" + this.north.isDestructable + ", East:"
+					+ this.east.isDestructable + ", South:"
+					+ this.south.isDestructable + ", West:"
+					+ this.west.isDestructable;
+		}
 	}
 
 	private static class Cell {
 
 		char sign = ' ';
 		Point position;
+		int cellNumber;
 		boolean isStart = false;
 		boolean isEnd = false;
 		int parent; // the parent in the disjointSet
+		int rank = 0; // the rank of the current node/tree
 		CellWalls cellWalls;
 		Cell_Type type;
 
@@ -225,11 +251,44 @@ public class Maze {
 
 		@Override
 		public String toString() {
-			return "Cell: " + position.toString();
+			return "Cell: " + position.toString() + cellWalls.toString();
 		}
 	}
 
 	private static class DisjointSet {
+
+		// no makeSet() function because i make the set independent of the
+		// DisjointSet class.
+
+		private static Cell findSet(int x) {
+			if (cellList[x].parent == x) {
+				System.out.println("FindSet: Cell " + cellList[x].cellNumber
+						+ "'s parent is: " + cellList[x].parent);
+				return cellList[x];
+			}
+
+			cellList[x].parent = findSet(cellList[x].parent).cellNumber;
+			return findSet(cellList[x].parent);
+		}
+
+		private static void union(int x, int y) {
+
+			Cell parentX = findSet(x);
+			Cell parentY = findSet(y);
+
+			if (parentX.rank < parentY.rank) {
+				cellList[parentX.cellNumber].parent = parentY.cellNumber;
+			} else if (parentY.rank < parentX.rank) {
+				cellList[parentY.cellNumber].parent = parentX.cellNumber;
+			} else {
+				cellList[parentY.cellNumber].parent = parentX.cellNumber;
+				cellList[parentX.cellNumber].rank++;
+			}
+
+			cellList[findSet(y).cellNumber].parent = findSet(x).cellNumber;
+			System.out.println("UNION: Cell " + cellList[y].cellNumber
+					+ "'s parent is now " + cellList[x].cellNumber);
+		}
 
 	}
 
